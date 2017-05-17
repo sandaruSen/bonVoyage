b15e78589bc95dc7661f5ab51ff69c6fd643b949
sandaruSen <sandaru.14@cse.mrt.ac.lk>
2017-05-18 01:00:24 +0530

    third commit

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ var updateTravel=require('./routes/updateTravel');
 var updateHotel=require('./routes/updateHotel');
 var app = express();
 
-// mongoose.connect('mongodb://127.0.0.1:27017/bonVoyageDatabase');
+//mongoose.connect('mongodb://127.0.0.1:27017/bonVoyageDatabase');
 mongoose.connect('mongodb://root:123@ds143131.mlab.com:43131/bonvoyagedatabase');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
