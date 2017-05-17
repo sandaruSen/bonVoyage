e94d6e4e207896589147e5ef91e64e737e5590f8
sandaruSen <sandaru.14@cse.mrt.ac.lk>
2017-05-18 01:38:05 +0530

    7th commit

--- routes/singIn.js
+++ routes/singIn.js
@@ -2,13 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-// router.get('/', function(req, res, next) {
-//     res.render('signIn', { title: 'SignIn-Admin' });
-//     // res.render('successful', { title: 'Signed' });
-// });
-
-router.post('/', function(req, res, next) {
-    console.log(req.body);
-    res.render('adminHome', { title: '' });
+router.get('/', function(req, res, next) {
+    res.render('signIn', { title: '' });
+    // res.render('successful', { title: 'Signed' });
 });
+//
+// router.post('/', function(req, res, next) {
+//     console.log(req.body);
+//     res.render('adminHome', { title: '' });
+// });
 module.exports = router;
